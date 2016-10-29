12ea7303fe3b11fa7b81c945df2900a2195036bc
mzp <mzpppp@gmail.com>
2016-10-29 19:18:07 +0900

    Initial Commit

---
+++ sushibar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  sushibar
+//
+//  Created by mzp on 2016/10/29.
+//  Copyright © 2016 mzp. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
