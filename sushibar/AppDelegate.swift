fc6bb2556ab5aceafbdecfec47bd932d90c385d9
mzp <mzpppp@gmail.com>
2016-10-29 20:19:42 +0900

    show button

--- sushibar/AppDelegate.swift
+++ sushibar/AppDelegate.swift
@@ -8,11 +8,18 @@
 
 import Cocoa
 
+fileprivate extension NSTouchBarItemIdentifier {
+    static let kome = NSTouchBarItemIdentifier("jp.mzp.touchbar.kome")
+    static let fish = NSTouchBarItemIdentifier("jp.mzp.touchbar.fish")
+    static let sushi = NSTouchBarItemIdentifier("jp.mzp.touchbar.sushi")
+}
+
+@available(OSX 10.12.1, *)
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, NSTouchBarProvider, NSTouchBarDelegate {
 
     @IBOutlet weak var window: NSWindow!
-
+    var touchBar: NSTouchBar?
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         window.title = "🍣"
@@ -26,12 +33,45 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             sushi.font = NSFont.systemFont(ofSize: 128)
             window.contentView?.addSubview(sushi)
         }
+
+        self.touchBar = makeTouchBar()
+    }
+
+
+    func makeTouchBar() -> NSTouchBar? {
+        let mainBar = NSTouchBar()
+        mainBar.delegate = self
+        mainBar.defaultItemIdentifiers = [.kome, .fish, .sushi]
+        return mainBar
+    }
+
+    func touchBar(_ touchBar: NSTouchBar, makeItemForIdentifier identifier: NSTouchBarItemIdentifier) -> NSTouchBarItem? {
+        if identifier == .kome {
+            let item = NSCustomTouchBarItem(identifier: identifier)
+            let button = NSButton(title: "🍚", target: self, action: #selector(AppDelegate.tapped(_:)))
+            item.view = button
+            return item
+        } else if identifier == .fish {
+            let item = NSCustomTouchBarItem(identifier: identifier)
+            let button = NSButton(title: "🐟", target: self, action: #selector(AppDelegate.tapped(_:)))
+            item.view = button
+            return item
+        } else if identifier == .sushi {
+            let item = NSCustomTouchBarItem(identifier: identifier)
+            let button = NSButton(title: "🍣", target: self, action: #selector(AppDelegate.tapped(_:)))
+            item.view = button
+            return item
+        } else {
+            return nil
+        }
+    }
+
+    @objc
+    func tapped(_ sender : NSObject) {
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
 }
 
