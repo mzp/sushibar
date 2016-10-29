79c54b915e5b8629a0ec750eb8e05d6cc298a7d7
mzp <mzpppp@gmail.com>
2016-10-29 19:46:06 +0900

    show :sushi: on window center

--- sushibar/AppDelegate.swift
+++ sushibar/AppDelegate.swift
@@ -15,7 +15,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        window.title = "🍣"
+
+        if let frame = window.contentView?.frame {
+            let sushi = NSTextView(frame: NSMakeRect(frame.size.width/2-64, frame.size.height/2-64, 150, 128))
+            sushi.string = "🍣"
+            sushi.drawsBackground = false
+            sushi.isEditable = false
+            sushi.isSelectable = false
+            sushi.font = NSFont.systemFont(ofSize: 128)
+            window.contentView?.addSubview(sushi)
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
